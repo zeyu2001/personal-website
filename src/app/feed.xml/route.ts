c97bfa3e636b1a6d08cd098cd64b598ad1ac52dd
Zeyu Zhang <39144422+zeyu2001@users.noreply.github.com>
2025-05-16 02:06:41 +0100

    feat: speed insights

--- src/app/feed.xml/route.ts
+++ src/app/feed.xml/route.ts
@@ -35,7 +35,7 @@ export async function GET(req: Request) {
     .map((key) => key.slice(2).replace(/\/page\.mdx$/, ''))
 
   for (let id of articleIds) {
-    let url = String(new URL(`/blog/${id}`, req.url))
+    let url = String(new URL(`/blog/${id}`, siteUrl))
     let html = await (await fetch(url)).text()
     let $ = cheerio.load(html)
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/styles/tailwind.css'
 
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { type Metadata } from 'next'
 
 import { Providers } from '@/app/providers'
@@ -29,6 +30,7 @@ export default function RootLayout({
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
       <body className="flex h-full bg-zinc-50 dark:bg-zinc-900">
         <Analytics />
+        <SpeedInsights />
         <Providers>
           <div className="flex w-full">
             <Layout>{children}</Layout>
